f2f9470f0d92b2b10229a02427dc465024b32e3a
Diego Luis <diego_bezerra95@hotmail.com>
2022-03-12 19:22:05 -0300

    Correçoes de erro no MovieStars

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -2,8 +2,6 @@ import { ReactComponent as StarFull} from 'assets/img/star-full.svg';
 import { ReactComponent as StarHalf} from 'assets/img/star-half.svg';
 import { ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
 import './styles.css';
-import { type } from 'os';
-
 
 type Props = {
   score: number;
@@ -52,8 +50,7 @@ function MovieStars({ score } : Props) {
             <Star fill={fills[1]}/>
             <Star fill={fills[2]}/>
             <Star fill={fills[3]}/>
-            <Star fill={fills[4]}/>
-          
+            <Star fill={fills[4]}/> 
         </div>
     );
 
